85a8300c0b9767665750549fdac5b8509853340a
Shivansh Gupta <shivanshgupta2607@outlook.com>
2025-07-08 18:37:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let res="";
 let win=0; let lose=0; let draw=0;
 let x = Math.random();
     if(x>=0 && x<1/3){
-        compMove="rock";
+        compMove="&#x270A;";
     } else if(x>=1/3 && x<2/3){
-        compMove="paper";
+        compMove="&#x1F590;";
     } else if(x>=2/3 && x<1){
-        compMove="scissor";
+        compMove="&#x270C;";
     }
 
 function reset() {
@@ -30,33 +30,33 @@ function start() {
 
     let x = Math.random();
     if(x>=0 && x<1/3){
-        compMove="rock";
+        compMove="&#x270A;";
     } else if(x>=1/3 && x<2/3){
-        compMove="paper";
+        compMove="&#x1F590;";
     } else if(x>=2/3 && x<1){
-        compMove="scissor";
+        compMove="&#x270C;";
     }
 
     btns.forEach(btn => btn.disabled = false);
     
     btns[0].onclick= function(){
-        userMove="rock";
-        document.querySelector("#um").innerText=`Your move : ${userMove.toUpperCase()}`;
-        document.querySelector("#cm").innerText=`Computer move : ${compMove.toUpperCase()}`;
-        if(compMove==="rock") {
+        userMove="&#x270A;";
+        document.querySelector("#um").innerHTML=`Your move : <br><span style="font-size:50px;"> ${userMove} </span>`;
+        document.querySelector("#cm").innerHTML=`Computer move : <br><span style="font-size:50px;"> ${compMove} </span>`;
+        if(compMove==="&#x270A;") {
             res="GAME IS DRAWN.";
             draw++;
         }
-        else if(compMove==="paper") {
+        else if(compMove==="&#x1F590;") {
             res="YOU LOSE!";
             lose++;
         }
-        else if(compMove==="scissor") {
+        else if(compMove==="&#x270C;") {
             res="YOU WON!";
             win++;
         }
         document.querySelector("#res").innerText=`${res}`;
-        setTimeout(start, 2500);
+        setTimeout(start, 3000);
         document.querySelector("#win").innerText=`WINS: ${win}`;
         document.querySelector("#lose").innerText=`LOSE: ${lose}`;
         document.querySelector("#draw").innerText=`DRAW: ${draw}`;
@@ -65,23 +65,23 @@ function start() {
     }
 
     btns[1].onclick= function(){
-        userMove="paper";
-        document.querySelector("#um").innerText=`Your move : ${userMove.toUpperCase()}`;
-        document.querySelector("#cm").innerText=`Computer move : ${compMove.toUpperCase()}`;
-        if(compMove==="paper") {
+        userMove="&#x1F590;";
+        document.querySelector("#um").innerHTML=`Your move : <br><span style="font-size:50px;"> ${userMove} </span>`;
+        document.querySelector("#cm").innerHTML=`Computer move : <br><span style="font-size:50px;"> ${compMove} </span>`;
+        if(compMove==="&#x1F590;") {
             res="GAME IS DRAWN.";
             draw++;
         }
-        else if(compMove==="scissor") {
+        else if(compMove==="&#x270C;") {
             res="YOU LOSE!";
             lose++;
         }
-        else if(compMove==="rock") {
+        else if(compMove==="&#x270A;") {
             res="YOU WON!";
             win++;
         }
         document.querySelector("#res").innerText=`${res}`;
-        setTimeout(start, 2500);
+        setTimeout(start, 3000);
         document.querySelector("#win").innerText=`WINS: ${win}`;
         document.querySelector("#lose").innerText=`LOSE: ${lose}`;
         document.querySelector("#draw").innerText=`DRAW: ${draw}`;
@@ -89,23 +89,23 @@ function start() {
     }
 
     btns[2].onclick= function(){
-        userMove="scissor";
-        document.querySelector("#um").innerText=`Your move : ${userMove.toUpperCase()}`;
-        document.querySelector("#cm").innerText=`Computer move : ${compMove.toUpperCase()}`;
-        if(compMove==="scissor") {
+        userMove="&#x270C;";
+        document.querySelector("#um").innerHTML=`Your move : <br><span style="font-size:50px;"> ${userMove} </span>`;
+        document.querySelector("#cm").innerHTML=`Computer move : <br><span style="font-size:50px;"> ${compMove} </span>`;
+        if(compMove==="&#x270C;") {
             res="GAME IS DRAWN.";
             draw++;
         }
-        else if(compMove==="rock") {
+        else if(compMove==="&#x270A;") {
             res="YOU LOSE!";
             lose++;
         }
-        else if(compMove==="paper") {
+        else if(compMove==="&#x1F590;") {
             res="YOU WON!";
             win++;
         }
         document.querySelector("#res").innerText=`${res}`;
-        setTimeout(start, 2500);
+        setTimeout(start, 3000);
         document.querySelector("#win").innerText=`WINS: ${win}`;
         document.querySelector("#lose").innerText=`LOSE: ${lose}`;
         document.querySelector("#draw").innerText=`DRAW: ${draw}`;
@@ -115,6 +115,7 @@ function start() {
     document.querySelector("#restart").onclick= function(){
         reset();
         start();
+        
     }
 }
 
